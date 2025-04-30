cbd0912f5cd2c22998a0011d9b34daeac6fc1a27
Thuyngan <tranttngann@gmail.com>
2025-05-01 00:20:21 +0700

    create funtion that allow to sort globalSale in ascending order

--- src/VideoGameSales.java
+++ src/VideoGameSales.java
@@ -1,37 +1,33 @@
 import java.io.*;
 import java.util.ArrayList;
-
+import java.util.Collections;
+import java.util.Comparator;
 
 public class VideoGameSales {
 
-    static ArrayList readFileIntoArraylist (String file) throws IOException {
+    static ArrayList readFileIntoArraylist(String file) throws IOException {
         BufferedReader reader = null;
-        String line ="";
+        String line = "";
 
         try {
             reader = new BufferedReader(new FileReader(file));
             ArrayList<gameSales> gameArray = new ArrayList<>();
-            int i=0;
-
+            int i = 0;
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
                 boolean checkNA = true;
                 if (row[3].equals("N/A"))
                     checkNA = false;
-                System.out.println(i);
-                if(i!=0 && checkNA) {
+                if (i != 0 && checkNA) {
                     gameSales abc = new gameSales(Integer.valueOf(row[0]), row[1], row[2], Integer.valueOf(row[3]), row[4], row[5], Double.valueOf(row[6]), Double.valueOf(row[7]), Double.valueOf(row[8]), Double.valueOf(row[9]), Double.valueOf(row[10]));
                     gameArray.add(abc);
                 }
-                i = i +1;
+                i = i + 1;
             }
             return gameArray;
-        }
-
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 reader.close();
             } catch (IOException e) {
@@ -41,7 +37,7 @@ public class VideoGameSales {
         return null;
     }
 
-    static class gameSales {
+    static class gameSales implements Comparable<gameSales> {
         int rank;
         String name;
         String platform;
@@ -71,44 +67,66 @@ public class VideoGameSales {
         int getRank() {
             return rank;
         }
+
         String getName() {
             return name;
         }
+
         String getPlatform() {
             return platform;
         }
+
         int getYear() {
             return year;
         }
+
         String getGenre() {
             return genre;
         }
+
         String getPublisher() {
             return publisher;
         }
+
         double getNAs() {
             return NAs;
         }
         double getEUs() {
             return EUs;
         }
+
         double getJPs() {
             return JPs;
         }
+
         double getOtherSales() {
             return otherSales;
         }
+
         double getGlobalSale() {
             return globalSale;
         }
+
+        public int compareTo(gameSales other) {
+            // Compare by grade (descending order)
+            if (Integer.compare(other.rank, this.rank) != 0) {
+                return Double.compare(other.rank, this.rank );
+            }
+            return 0;
+        }
     }
 
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         ArrayList<gameSales> gameList = readFileIntoArraylist("src//videoGamesSales.csv");
-        for (int a = 0 ; a< gameList.size(); a++) {
-            System.out.println(gameList.get(a).getRank() + gameList.get(a).getName() + gameList.get(a).getPlatform() + gameList.get(a).getYear() + gameList.get(a).getGenre() + gameList.get(a).getPublisher() + gameList.get(a).getNAs() + gameList.get(a).getEUs() + gameList.get(a).getJPs() + gameList.get(a).getOtherSales() + gameList.get(a).getGlobalSale());
+
+        Collections.sort(gameList);
+        for (int a = 0; a < gameList.size(); a++) {
+            System.out.println(gameList.get(a).getRank() + " " + gameList.get(a).getName() + " " + gameList.get(a).getGlobalSale());
+
         }
     }
 }
+
+
 
