f2371b28c2dfe180fcdc96ae5dec3dfca38e9d21
Thuyngan <tranttngann@gmail.com>
2025-05-03 13:29:24 +0700

    create and output data to a csv file

--- src/abc.java
+++ src/abc.java
@@ -1,60 +1,103 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.lang.reflect.Field;
 
 
 public class abc {
-    static class Student implements Comparable<Student> {
-       String name;
-       int age;
-       double grade;
 
-        Student(String name, int age, double grade) {
+    /*
+    public static void writeCsv(String filePath, ArrayList<students> detail) {
+        File csvFile = new File(filePath);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile))) {
+            for (String[] row : data) {
+                StringBuilder line = new StringBuilder();
+                for (int i = 0; i < row.length; i++) {
+                    line.append(escapeSpecialCharacters(row[i]));
+                    if (i < row.length - 1) {
+                        line.append(",");
+                    }
+                }
+                writer.write(line.toString());
+                writer.newLine();
+            }
+        } */
+    public static void writeCsv(String filePath, ArrayList<students> detail) {
+        File csvFile = new File(filePath);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile))) {
+            for (students row : detail) {
+               // writer.write(row.toString());
+                StringBuilder line = new StringBuilder();
+                line.append(students.loopThroughAttributes(row));
+                writer.write(line.toString());
+                writer.newLine();
+            }
+        } catch (IllegalAccessException | IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+    static  class students {
+        String name;
+        String age;
+        String gender;
+        String height;
+
+        students (String name, String age, String gender, String height) {
             this.name = name;
             this.age = age;
-            this.grade = grade;
+            this.gender = gender;
+            this.height = height;
+        }
+
+        public static Object loopThroughAttributes(Object obj) throws IllegalAccessException {
+            Class<?> clazz = obj.getClass();
+            Field[] fields = clazz.getDeclaredFields();
+            StringBuilder line = new StringBuilder();
+            for (Field row : fields) {
+                row.setAccessible(true);
+                line.append(row.get(obj));
+            }
+            return obj;
+        }
+
+        @Override
+        public String toString() {
+            return name + "," + age + "," + gender + "," + height;
+        }
+
+        public static int getColumnCount() {
+            return students.class.getDeclaredFields().length;
         }
 
         String getName() {
             return name;
         }
-        int getAge() {
+        String getAge() {
             return age;
         }
-        double getGrade() {
-            return grade;
+        String getGender() {
+            return gender;
         }
-
-        public int compareTo(Student other) {
-            // Compare by grade (descending order)
-            if (Double.compare(other.grade, this.grade) != 0) {
-                return Double.compare(this.grade, other.grade) ;
-            }
-            return 0;
+        String getHeight() {
+            return height;
         }
+    }
 
-        public static void main(String[] args) {
-            // Create a list of students
-            ArrayList<Student> students = new ArrayList<>();
-            students.add(new Student("Alice", 20, 85.5));
-            students.add(new Student("Bob", 22, 90.0));
-            students.add(new Student("Charlie", 19, 78.3));
-            students.add(new Student("David", 21, 85.5));
-            students.add(new Student("Eve", 20, 92.7));
+    public static void main(String[] args) {
+        ArrayList<students> detail = new ArrayList<>();
+        detail.add(new students("Ngan", "15", "female", "1.52"));
+        detail.add(new students("Chinh", "25", "male", "1.67"));
+        detail.add(new students("Hien", "18", "female", "1.60"));
+        detail.add(new students("Thanh", "23", "male", "1.72"));
+        detail.add(new students("Yen", "30", "female", "1.62"));
 
-            // Display unsorted list
-            System.out.println("Unsorted list of students:");
-            for (int i=0; i<students.size(); i++) {
-                System.out.println(students.get(i).getName() + " " + students.get(i).getAge() +" " + students.get(i).getGrade());
-            }
-
-            // Sort the list using the natural ordering defined by Comparable
-            Collections.sort(students);
-
-            // Display sorted list
-            System.out.println("\nSorted list of students (by grade descending, then age ascending, then name):");
-            for (int i=0; i<students.size(); i++) {
-                System.out.println(students.get(i).getName() + " " + students.get(i).getAge() + " " + students.get(i).getGrade());
-            }
-        }
+        String filePath = "C:\\Users\\admin\\Coding_work\\videoGameSales\\out\\output.csv";
+        writeCsv(filePath, detail);
+        System.out.println("CSV file created successfully: " + filePath);
     }
 }
--- src/helloworld.java
+++ src/helloworld.java
@@ -1,78 +1,34 @@
-
-import java.util.*;
-import java.util.Comparator;
+import java.lang.reflect.Field;
 
 public class helloworld {
-    static class circle {
-        String name;
-        double radius;
-        int price;
 
-        circle(String name, double radius, int price) {
-            this.name = name;
-            this.radius = radius;
-            this.price = price;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        double getRadius() {
-            return radius;
-        }
-
-        int getPrice() {
-            return price;
-        }
-
-        public static Comparator<circle> compareName = new Comparator<circle>() {
-            public int compare(circle s1, circle s2) {
-                String name1 = s1.getName();
-                String name2 = s2.getName();
-
-                // Returning in ascending order
-                return name1.compareTo(name2);
-            }
-        };
-
-        // Comparator for sorting the list by roll no
-        public static Comparator<circle> compareRadius = new Comparator<circle>() {
-            public int compare(circle s1, circle s2) {
-
-                double radius1 = s1.getRadius();
-                double radius2 = s2.getRadius();
-                int value1 = (int)radius1;
-                int value2 = (int)radius2;
-
-                // For ascending order
-                return value2 - value1 ;
-            }
-        };
-
-        @Override
-        public String toString() {
-            return "[ radius=" + radius + ", name=" + name + ", price=" + price + "]";
-        }
+    public static void main(String[] args) throws IllegalAccessException {
+        MyClass obj = new MyClass("example", "42", "45" );
+        loopThroughAttributes(obj);
     }
 
-    public static void main(String[] args) {
+    public static void loopThroughAttributes(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
 
-        ArrayList<circle> abc = new ArrayList<circle>();
-        abc.add(new circle("a", 5.3, 23));
-        abc.add(new circle("b", 4.5, 56));
-        abc.add(new circle("c", 7.8, 13));
-
-        System.out.println("Circle Name Sorting:");
-        Collections.reverse(abc);
-        for (circle str : abc) {
-            System.out.println(str);
+        for (Field field : fields) {
+            field.setAccessible(true); // Allow access to private fields
+            String name = field.getName();
+            Object value = field.get(obj);
+            System.out.println("Attribute: " + name + ", Value: " + value);
         }
+    }
+}
 
-        System.out.println("Radius Sorting:");
-        Collections.sort(abc, circle.compareRadius);
-        for (circle str : abc) {
-            System.out.println(str);
-        }
+class MyClass {
+    String text;
+    String number;
+    String age;
+
+
+    public MyClass(String text, String number, String age) {
+        this.text = text;
+        this.number = number;
+        this.age = age;
     }
 }
